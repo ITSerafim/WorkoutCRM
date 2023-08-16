addd16c2df20e4c292b4e6374d883687db879607
ITSerafim <aserafimow@gmail.com>
2023-08-16 16:01:24 +0300

    WORKOUT-TABLE: Добавил общий заголовок для всех разделов

--- src/pages/WorkoutPage.vue
+++ src/pages/WorkoutPage.vue
@@ -1,7 +1,10 @@
 <template>
-    <MainLayout></MainLayout>
+  <MainLayout>
+    <PageHeader header="Тренировки"/>
+  </MainLayout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from '../layouts/MainLayout.vue';
+import PageHeader from '../shared/components/page-header/PageHeader.vue';
 </script>
