6823b9a91af7d9c07031441aa6af5c8af22db59b
ITSerafim <aserafimow@gmail.com>
2023-08-16 17:05:52 +0300

    WORKOUT-TABLE: Сверстал таблицу тренеровок

---
+++ src/components/workout/WorkoutTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th v-for="(column, idx) of props.columns" :key="idx">{{ column }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-if="data?.length === 0">
+        <td>{{ emptyMessage }}</td>
+      </tr>
+      <tr v-else>
+        <td v-for="(item, idx) of props.data" :key="idx">{{ item }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup lang="ts">
+interface TableProps {
+  columns: string[];
+  emptyMessage: string;
+  data?: unknown[];
+}
+
+const props = defineProps<TableProps>();
+
+</script>
+
--- src/pages/WorkoutPage.vue
+++ src/pages/WorkoutPage.vue
@@ -1,10 +1,28 @@
 <template>
   <MainLayout>
-    <PageHeader header="Тренировки"/>
+    <PageHeader header="Тренировки" />
+    <WorkoutTable :columns="columns" :data="data" empty-message="Тренировки отсутствуют" />
   </MainLayout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from '../layouts/MainLayout.vue';
 import PageHeader from '../shared/components/page-header/PageHeader.vue';
+import WorkoutTable from '../components/workout/WorkoutTable.vue';
+
+const columns: string[] = [
+  'Название тренировок',
+  'Описание тренировок',
+  'Дата тренировок',
+  'Кол-во подходов',
+  'Кол-во кругов',
+  'Кол-во упражнений',
+  'Время отдыха между кругами',
+  'Время отдыха между подходами',
+  'Время отдыха между упражнениями'
+];
+
+
+const data: unknown[] = [
+];
 </script>
