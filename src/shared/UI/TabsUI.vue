dfb0da40d4525d60d1aca6b899cf7477f62cc015
ITSerafim <aserafimow@gmail.com>
2023-08-16 17:47:02 +0300

    WORKOUT-TABLE: Добавил компонент табов

---
+++ src/shared/UI/TabsUI.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="tabs">
+    <router-link
+      v-for="(route, idx) in props.routes"
+      :key="idx"
+      active-class="active"
+      :to="route.path"
+      class="tabs-el"
+    >
+      {{ route.name }}
+    </router-link>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { Route } from '../types/Route';
+
+interface TabsProps {
+  routes: Route[];
+}
+
+const props = defineProps<TabsProps>();
+</script>
