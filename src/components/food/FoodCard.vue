a1e6b453952396a73136b1a363e2ff3de282ea6c
aserafimov <aserafimow@gmail.com>
2023-09-14 17:59:07 +0300

    FOOD: Сверстал раздел 'Питание'

---
+++ src/components/food/FoodCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-wrapper">
+    <img src="/src/assets/food-template.png" alt="" class="card-img" />
+    <div class="card-info">
+      <h4 class="card-title card-text">{{ title }}</h4>
+      <span class="card-type card-text">
+        Тип: <span class="card-blue">{{ type }}</span>
+      </span>
+      <span class="card-kind card-text">
+        Вид спорта: <span class="card-blue">{{ sportKind }}</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface FoodCardProps {
+  title: string;
+  type: string;
+  sportKind: string;
+}
+
+defineProps<FoodCardProps>();
+</script>
---
+++ src/pages/FoodPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <MainLayout>
+    <PageHeader
+      header="Питание"
+      share-name="планом питания"
+      create-name="план питания"
+    />
+    <TabsUI :routes="tabs" />
+    <RouterView></RouterView>
+  </MainLayout>
+</template>
+
+<script setup lang="ts">
+import MainLayout from '../layouts/MainLayout.vue';
+import TabsUI from '../shared/UI/TabsUI.vue';
+import PageHeader from '../shared/components/page-header/PageHeader.vue';
+import { Route } from '../shared/types/Route';
+
+const tabs: Route[] = [
+  {
+    path: '/food/all',
+    name: 'Планы питания',
+  },
+];
+</script>
