d8e55cbc508e9a9aa62918cc78833bc865fb94c1
serafimow <aserafimow@gmail.com>
2023-10-21 23:29:44 +0300

    WORKOUT-2747: Добавил два контрола и прототип базовой формы

--- src/components/workout-page/workout/WorkoutCreateForm.vue
+++ src/components/workout-page/workout/WorkoutCreateForm.vue
@@ -1,7 +1,8 @@
 <template>
   <form class="workout-form">
-    <InputUI v-model="workout.name" label="Название тренировки" />
-    <TextareaUI v-model="workout.description" label="Описание тренировки" />
+    <!-- <InputUI v-model="workout.name" label="Название тренировки" />
+    <TextareaUI v-model="workout.description" label="Описание тренировки" /> -->
+    <BaseForm :config="config" />
     <div class="workout-form__grid">
       <InputWithoutFloatingUI
         v-model="workout.cyclesCount"
@@ -21,23 +22,36 @@
       <ButtonUI
         type="submit"
         class="login"
-        @click.prevent="$emit('create', { ...workout, dateWorkout })"
+        @click.prevent="console.log(config[0].value)"
       >
         Создать тренировку
       </ButtonUI>
     </div>
   </form>
+  <!-- $emit('create', { ...workout, dateWorkout }) -->
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-
-import TextareaUI from '../../../shared/UI/TextareaUI.vue';
 import InputWithoutFloatingUI from '../../../shared/UI/InputWithoutFloatingUI.vue';
 import { Workout } from '../../../models/Workout';
 import ButtonUI from '../../../shared/UI/ButtonUI.vue';
-import InputUI from '../../../shared/UI/InputUI.vue';
 import DatePickerUI from '../../../shared/UI/DatePickerUI.vue';
+import BaseForm from '../../../shared/components/base-form/BaseForm.vue';
+
+const config = [
+  {
+    name: 'inputText',
+    label: 'Test1',
+    value: '',
+  },
+
+  {
+    name: 'textareaText',
+    label: 'Test2',
+    value: '',
+  },
+];
 
 const workout = ref<Workout>({
   name: '',
---
+++ src/shared/components/base-form/BaseForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-for="(control, idx) of config" :key="idx">
+    <component
+      :is="controlsMap[control.name]"
+      v-model="control.value"
+      :label="control.label"
+    ></component>
+  </div>
+</template>
+
+<script setup lang="ts">
+import InputFormControl from './controls/InputFormControl.vue';
+import TextareaFormControl from './controls/TextareaFormControl.vue';
+
+const controlsMap: { [key: string]: any } = {
+  inputText: InputFormControl,
+  textareaText: TextareaFormControl,
+};
+
+interface BaseFormProps {
+  config: any;
+}
+
+defineProps<BaseFormProps>();
+</script>
