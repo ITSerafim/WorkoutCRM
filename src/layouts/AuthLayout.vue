77a915c69257587dc95fbe6b6d412fdaa0e50c3d
serafimow <aserafimow@gmail.com>
2023-11-14 10:28:03 +0300

    WORKOUT-2711: Переделал страницу авторизации

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,7 +1,11 @@
 <template>
-  <main class="auth-layout">
-    <img :src="`src/assets/${image}`" alt="main picture" class="image" />
-    <section class="auth-section">
+  <main class="grid col-2 relative">
+    <img
+      :src="`src/assets/${image}`"
+      alt="main picture"
+      class="wi-100 he-100"
+    />
+    <section class="flex flex-col jc-space p-20">
       <slot></slot>
     </section>
   </main>
