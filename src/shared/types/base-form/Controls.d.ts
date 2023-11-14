6aee98710059f6167dcfd9a0d7194676b366691a
serafimow <aserafimow@gmail.com>
2023-11-14 10:27:44 +0300

    WORKOUT-2711: Расширил базовую форму

--- src/shared/types/base-form/Controls.d.ts
+++ src/shared/types/base-form/Controls.d.ts
@@ -2,10 +2,21 @@ export interface Control {
   control: string;
   name: string;
   label: string;
-  value?: string | number | Date;
+  withIcon?: boolean;
+  iconName?: string;
+  controlStyles?: string;
+  linkPath?: string;
+  value?: ControlValue;
   callback?: () => void;
 }
 
 export interface Controls {
   [key: string]: unknown;
 }
+
+export interface Content {
+  html: string;
+  slotName: string;
+}
+
+type ControlValue = string | number | boolean | Date;
