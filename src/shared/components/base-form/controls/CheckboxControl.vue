6c8dd67cbfe43971604e8c6379ca8c9193c2f6f6
serafimow <aserafimow@gmail.com>
2023-11-14 10:27:19 +0300

    WORKOUT-2711: Добавил два новых контрола, обновил button control

---
+++ src/shared/components/base-form/controls/CheckboxControl.vue
@@ -0,0 +1,16 @@
+<template>
+  <CheckboxUI v-model="value" :label="label"></CheckboxUI>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import CheckboxUI from '../../../UI/CheckboxUI.vue';
+
+interface CheckboxControlProps {
+  label: string;
+}
+
+defineProps<CheckboxControlProps>();
+
+const value = ref<boolean>(false);
+</script>
